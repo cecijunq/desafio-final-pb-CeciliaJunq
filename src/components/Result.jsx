af3b3a22f6b476dc3b3a6024c045554ed01cfffc
Cecília Junqueira <cicijunqueira@hotmail.com>
2022-04-21 20:27:37 -0300

    Primeiro commit

---
+++ src/components/Result.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h2>Result</h2>
+        );
+    }
+}
+ 
+export default Result;
